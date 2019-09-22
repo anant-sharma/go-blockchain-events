c6665c0965724c9dc7fec3e7732833e3c08f418e
Anant Sharma <anantatnet@gmail.com>
2019-09-22 22:41:03 +0530

    Modified to persistent queue

--- controller/v1/events-listener/eventslistener.go
+++ controller/v1/events-listener/eventslistener.go
@@ -1,32 +1,75 @@
 package eventslistener
 
 import (
-	"github.com/anant-sharma/go-blockchain-events/models"
+	"encoding/json"
+	"log"
 
-	utils "github.com/anant-sharma/go-blockchain/common"
+	"github.com/anant-sharma/go-blockchain-events/models"
 	"github.com/anant-sharma/go-blockchain/controller/v1/blockchain"
 	"github.com/anant-sharma/go-blockchain/controller/v1/pubsub"
 	"github.com/mitchellh/mapstructure"
+
+	config "github.com/anant-sharma/go-blockchain-config"
+	"github.com/anant-sharma/go-utils/mq"
+	"github.com/streadway/amqp"
 )
+
+func createQueue(exchange string, queue string) mq.MQ {
+	_mq := mq.NewMQ()
+
+	// Connect to MQ
+	_mq.Connect(config.GetConfig().MQConnectionString)
+
+	// Create Channel with MQ
+	_mq.CreateChannel()
+
+	// Create Exchange
+	_mq.CreateExchange(exchange, "fanout", true)
+
+	// Create Queue
+	_mq.CreateQueue(queue, true, false, false, false, amqp.Table{})
+	log.Printf("[*] Queue %s Created.", queue)
+
+	// Bind Queue With Exchange
+	_mq.BindQueueWithExchange(queue, exchange, "", amqp.Table{})
+	log.Printf("[*] Queue %s Bound To Exchange %s.", queue, exchange)
+
+	// Create Exchange Log Queue
+	_mq.CreateQueue(exchange+"-logs", true, false, false, false, amqp.Table{
+		"x-message-ttl": int(86400),
+	})
+
+	// Bind Exchange Log Queue
+	_mq.BindQueueWithExchange(exchange+"-logs", exchange, "", amqp.Table{
+		"x-message-ttl": int64(86400),
+	})
+
+	return _mq
+}
 
 // InitListening - Start
 func InitListening() {
 
-	// Init PubSub
-	pubsub.NewPubSub("bc.msg.exchange", utils.GenerateUUID())
+	_mq := createQueue("bc.msg.exchange", "bc.events.queue")
 
 	// Add Event Subscribers
-	messages := pubsub.Subscribe()
+	messages := _mq.EstablishWorker("bc.events.queue")
 
-	for msg := range messages {
+	for d := range messages {
+		if d.Body != nil {
+			var msg pubsub.Message
+			json.Unmarshal(d.Body, &msg)
 
-		switch msg.Event {
-		case pubsub.PubSubEvents.TransactionCreated:
-			{
-				var transaction blockchain.Transaction
-				mapstructure.Decode(msg.Data, &transaction)
-				models.SaveTransaction(transaction)
+			switch msg.Event {
+			case pubsub.PubSubEvents.TransactionCreated:
+				{
+					var transaction blockchain.Transaction
+					mapstructure.Decode(msg.Data, &transaction)
+					models.SaveTransaction(transaction)
+				}
 			}
+
+			log.Printf("[x] %s", d.Body)
 		}
 	}
 }
